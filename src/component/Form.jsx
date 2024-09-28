64e27e06038b61ba29fb987c3c3890e101fe6a41
hasanraza38 <92hasanraza689@gmail.com>
2024-09-29 01:52:23 +0500

    completed

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,67 +1,142 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 function Form() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example"));
+
   return (
     <div className="flex justify-center items-center flex-col ">
-      <div className="flex justify-center gap-14 flex-wrap">
-        <input
-          type="text"
-          placeholder="First name"
-          className="input input-bordered input-lg w-full max-w-xs"
-        />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex justify-center gap-12 flex-wrap">
+          <div className="w-80">
+            <input
+              type="text"
+              placeholder="First name"
+              className="input input-bordered input-lg w-full max-w-xs"
+              {...register("first_name", { required: true , minLength: 4})}
+            />
+            <br />
+            {errors.first_name  && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
 
-        <input
-          type="text"
-          placeholder="Middle Name"
-          className="input input-bordered input-lg w-full max-w-xs"
-        />
+          <div className="w-80">
+            <input
+              type="text"
+              placeholder="Middle name"
+              className="input input-bordered input-lg w-full max-w-xs"
+              {...register("middle_name", { minLength:3})}
+            />
+            <br />
+            {errors.last_name  && (
+              <span className="text-zinc-300 font-medium">Optional</span>
+            )}
+            
+          </div>
 
-        <input
-          type="text"
-          placeholder="Last Name"
-          className="input input-bordered input-lg w-full max-w-xs"
-        />
 
-        <input
-          type="text"
-          placeholder="Email"
-          className="input input-bordered input-lg w-full max-w-xs"
-        />
+          <div className="w-80">
+            <input
+              type="text"
+              placeholder="Last name"
+              className="input input-bordered input-lg w-full max-w-xs"
+              {...register("last_name", { required: true })}
+            />
+            <br />
+            {errors.last_name  && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
 
-        <input
-          type="text"
-          placeholder="Phone"
-          className="input input-bordered input-lg w-full max-w-xs"
-        />
 
-        <input
-          type="text"
-          placeholder="CNIC Number"
-          className="input input-bordered input-lg w-full max-w-xs"
-        />
+          <div className="w-80">
+            <input
+              type="number"
+              placeholder="Age"
+              className="input input-bordered input-lg w-full max-w-xs"
+              {...register("age", { required: true })}
+            />
+            <br />
+            {errors.age  && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
 
-        <input
-          type="text"
-          placeholder="Address"
-          className="input input-bordered input-lg w-full max-w-xs"
-        />
 
-        <input
-          type="text"
-          placeholder="Education"
-          className="input input-bordered input-lg w-full max-w-xs"
-        />
+          <div className="w-80">
+            <input
+              type="email"
+              placeholder="Email"
+              className="input input-bordered input-lg w-full max-w-xs"
+              {...register("email", { required: true })}
+            />
+            <br />
+            {errors.email  && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
 
-        <input
-          type="text"
-          placeholder="Age"
-          className="input input-bordered input-lg w-full max-w-xs"
-        />
 
-        <button className="btn btn-wide bg-blue-400 text-white hover:bg-blue-500">
-          Submit
-        </button>
-      </div>
+          <div className="w-80">
+            <input
+              type="number"
+              placeholder="Phone"
+              className="input input-bordered input-lg w-full max-w-xs"
+              {...register("number", { required: true })}
+            />
+            <br />
+            {errors.number  && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
+
+
+          <div className="w-80">
+            <input
+              type="number"
+              placeholder="CNIC"
+              className="input input-bordered input-lg w-full max-w-xs"
+              {...register("cnic", { required: true })}
+            />
+            <br />
+            {errors.cnic  && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
+
+          <div className="w-80">
+            <input
+              type="text"
+              placeholder="Address"
+              className="input input-bordered input-lg w-full max-w-xs"
+              {...register("address", { required: true })}
+            />
+            <br />
+            {errors.address  && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
+
+          
+        </div>
+        <div className="flex justify-center mt-10">
+
+          <button className="btn btn-wide bg-blue-400 text-white hover:bg-blue-500">
+            Submit
+          </button>
+          </div>
+
+      </form>
     </div>
   );
 }
